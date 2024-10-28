4c3d73b7a2ff51de16099f4943a2d7407bd3f3bd
rol506 <rolromva@gmail.com>
2024-10-28 10:03:09 +0200

    Added glad and glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-  std::cout << "Hello from 3D Game!\n";
-  return 0;
+  if (!glfwInit())
+  {
+    std::cout << "glfwInit failed!\n";
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "3DGame by rol506", NULL, NULL);
+  if (!window)
+  {
+    std::cout << "failed to create window!\n";
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGL())
+  {
+    std::cout << "Failed to load GLaD\n";
+    glfwTerminate();
+    return 1;
+  }
+
+  std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+  std::cout << "OpenGL renderer: " << glGetString(GL_RENDERER) << "\n";
+
+  {
+    while (!glfwWindowShouldClose(window))
+    {
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      {
+        glfwSetWindowShouldClose(window, true);
+      }
+
+      glfwSwapBuffers(window);
+
+      glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+  }
 }
